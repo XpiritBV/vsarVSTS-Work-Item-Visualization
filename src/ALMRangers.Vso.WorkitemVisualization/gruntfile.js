f25d39d47bfe89efcfc778b115997d8d22cd7482
mattias sköld <mattias_skold71@hotmail.com>
2016-11-20 17:41:05 +0000

    Restructure to support Adding Concat, MInifification

---
+++ src/ALMRangers.Vso.WorkitemVisualization/gruntfile.js
@@ -0,0 +1,75 @@
+/// <binding AfterBuild='exec:package' />
+/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        exec: {
+            package: {
+                command: "tfx extension create --manifest-globs vss-extension.json",
+                stdout: true,
+                stderr: true
+            },
+            publish: {
+                command: "tfx extension publish  --manifest-globs vss-extension.json --publisher #mypublisher# --token #token#",
+                stdout: true,
+                stderr: true
+            }
+        },
+        jasmine: {
+            src: ["scripts/**/*.js", "sdk/**/*.js"],
+            specs: "test/**/*[sS]pec.js",
+            helpers: "test/helpers/*.js"
+        }, 
+        uglify: {
+            options: {
+                mangle: true
+            },
+            my_target: {
+                files: {
+                    'app/js/WorkItemVisualization.js': ['app/js/WorkItemVisualization.concat.js']
+                }
+            }
+            
+        },
+        concat: {
+            options: {
+                stripBanners: true,
+                process: function (src, filepath) {
+                    var modName = filepath.substring(filepath.indexOf("/app/js"));
+                    modName = modName.substring(0, filepath.indexOf(".js"));
+                    return src.replace('define(', 'define("' + modName + '", ');
+                },
+
+            },
+            dist: {
+                src: [
+                "app/js/AddEditHighlightDialog.js",
+                "app/js/AnnotationForm.js",
+                "app/js/FindWitDialog.js",
+                "app/js/LegendGrid.js",
+                "app/js/LegendMenu.js",
+                "app/js/MainMenu.js",
+                "app/js/PrintGraph.js",
+                "app/js/TelemetryClient.js",
+                "app/js/VsoStoreService.js",
+                "app/js/WorkItemVisualizationGrpah.js",
+                "app/js/WorkItemVisualization.js",
+                "app/js/WorkItemVisualizationApp.js",
+                ],
+                dest: 'app/js/WorkItemVisualization.concat.js'
+
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks("grunt-exec");
+    grunt.loadNpmTasks("grunt-contrib-jasmine");
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    
+
+};
